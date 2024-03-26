dbd3dd73bf4fe941a3e168911c9064d98b5ea869
Nathan Roos <nathan.roos@telecom-paris.fr>
2024-03-26 08:48:40 +0100

    Reformating to fit PEP8 recommandation

--- src/communication_netbox/query_generator.py
+++ src/communication_netbox/query_generator.py
@@ -13,32 +13,56 @@ from sgqlc.types import Type, Field, list_of
 from sgqlc.operation import Operation
 
 
-"""Declaration of the data types corresponding to the GraphQL schema of Netbox
-(only the types needed for the queries are declared)"""
+#Declaration of the data types corresponding to the GraphQL schema of Netbox
+# (only the types needed for the queries are declared)
 
 class Tag(Type):
+    """A tag is a label that can be attached to any object in Netbox
+    
+    Args:
+        Type (Type): MotherClass to create type of objects in Netbox"""
     name = str
 
 class IpAddressType(Type):
+    """IP address in Netbox
+    
+    Args:
+        Type (Type): MotherClass to create type of objects in Netbox"""
     address = str
     tags = list_of(Tag)
 
 class WirelessLAN(Type):
+    """Wireless LAN in Netbox
+    
+    Args:
+        Type (Type): MotherClass to create type of objects in Netbox"""
     ssid = str
     auth_psk = str
 
 class Interface(Type):
+    """Interface in Netbox (can be a physical interface or a virtual one like a VLAN)
+    
+    Args:
+        Type (Type): MotherClass to create type of objects in Netbox"""
     name = str
     tags = list_of(Tag)
     ip_addresses = list_of(IpAddressType)
     wireless_lans = list_of(WirelessLAN)
     
 class Query(Type):
+    """encompasses all the queries that can be made on the Netbox API
+    
+    Args:
+        Type (Type): MotherClass to create type of objects in Netbox"""
     interface_list = Field(list_of('Interface'), args={'mac_address': str})
 
 
 def camel_to_snake(string : str):
-    """convert a camelCase string to snake_case while preserving the integrity of the MAC addresses"""
+    """convert a camelCase string to snake_case while preserving
+      the integrity of the MAC addresses in the string
+      
+    Args :
+        string (str) : the string to convert to snake_case"""
     result = []
     is_previous_previous_colon = False
     is_previous_colon = False
@@ -48,12 +72,15 @@ def camel_to_snake(string : str):
             result.append(c.lower())
         else :
             is_previous_previous_colon = is_previous_colon
-            is_previous_colon = (c == ':')
+            is_previous_colon = c == ':'
             result.append(c)
     return string[0] + ''.join(result)
 
 def create_query(mac : str) -> str :
-    """create a query string to get the informations about a box from its MAC address"""
+    """create a query string to get the informations about a box from its MAC address
+    
+    Args :
+        mac (str) : the MAC address of the box"""
     query = Operation(Query)
     interfaces = query.interface_list(mac_address=mac)
     #selection of the fields to be returned
@@ -68,7 +95,5 @@ def create_query(mac : str) -> str :
     interfaces.wireless_lans.auth_psk()
     #Netbox API understands snake_case but the sgqlc library uses camelCase for the fields
     # so we convert it
-    snakified_query = camel_to_snake(str(query)) 
+    snakified_query = camel_to_snake(str(query))
     return snakified_query
-
-#ceci_est_un_motdepasse0
