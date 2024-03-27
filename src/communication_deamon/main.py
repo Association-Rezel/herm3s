97458eb9f06bfe858761e64a1432ad758fca4f64
Lefebvre <leo.lefebvre@telecom-paris.fr>
2024-03-27 22:48:10 +0100

    clearing code + adding comments

--- src/communication_deamon/main.py
+++ src/communication_deamon/main.py
@@ -1,34 +1,11 @@
 from netaddr import EUI
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-
-#put into other file
-from re import match
 from fastapi import HTTPException
 
-
-app = FastAPI()
-
-#put into other file
-def is_mac(mac:str):
-    standard_mac=r'([a-fA-F0-9]{2}([:-][a-fA-F0-9]{2}){5})'
-    if match(standard_mac,mac):
-        return True
-    return False
-class MacAddress1:
-    mac = None
-    def __init__(self, mac:str):
-        standard_mac = r'([a-fA-F0-9]{2}([:-][a-fA-F0-9]{2}){5})'
-        if match(standard_mac,mac):
-            self.mac = mac
-        else:
-            raise HTTPException(400, {"Erreur": "MAC invalide."})
-        
-    def getMac(self):
-        return self.mac
-
+#class to check if the mac address is valid and store it
 class MacAddress:
-    mac = None
+    mac : str = None;
     def __init__(self, mac:str):
         try:
             self.mac = str(EUI(mac)).replace("-", ":")
@@ -37,13 +14,15 @@ class MacAddress:
     def getMac(self):
         return self.mac
 
-#download file from server to client
+
+app = FastAPI()
+
+#download file from hermes to box
 @app.get("/box/{mac}/config",)
 
 async def get_file_config_init(mac: str):
     mac_box = MacAddress(mac)
     file = "spécification.txt"
-    print(mac_box.getMac())
     if not mac_box.getMac() is None:
         return FileResponse(file, filename=file)
     else:
