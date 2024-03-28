bc6ab693427a075c44377af20e27d419ff01dced
Lefebvre <leo.lefebvre@telecom-paris.fr>
2024-03-28 23:19:55 +0100

    adding api doc

--- src/communication_deamon/main.py
+++ src/communication_deamon/main.py
@@ -2,6 +2,7 @@ from netaddr import EUI
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 #class to check if the mac address is valid and store it
 class MacAddress:
@@ -17,14 +18,20 @@ class MacAddress:
 
 app = FastAPI()
 
+# Enable CORS (for swagger)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #download file from hermes to box
 @app.get("/box/{mac}/config",)
-
 async def get_file_config_init(mac: str):
     mac_box = MacAddress(mac)
     file = "spécification.txt"
     if not mac_box.getMac() is None:
         return FileResponse(file, filename=file)
     else:
-        raise HTTPException(404, {"Erreur": "MAC invalide."}) 
-    
+        raise HTTPException(404, {"Erreur": "MAC invalide."})
