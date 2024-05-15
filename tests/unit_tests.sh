a3979d527867d124aef384b66362d5031538ce70
Léo Lefebvre <leo.lefebvre@telecom-paris.fr>
2024-05-15 15:42:18 +0200

    update unit tests

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
@@ -8,13 +8,38 @@ NC='\033[0m'
 
 echo -e "Beginning unit tests..."
 
+sleep 3
+
 # ----------------------------------------
-# Unit tests 1
+# Unit tests 1: curl on '/'
 # ----------------------------------------
 echo -e "${YELLOW}Running unit test 1: curl on '/'${NC}"
-result=$(curl -s -o /dev/null -w "%{http_code}" http://api_hermes:8000/)
-if [ $result -eq 200 ]; then
-    echo -e "${GREEN}Unit test 1 passed with code ${result} !${NC}"
+http_code=$(curl -s -o /dev/null -w "%{http_code}" http://api_hermes:8000/)
+if [ $http_code -eq 200 ]; then
+    echo -e "${GREEN}Unit test 1 passed with code ${http_code} !${NC}"
 else
-    echo -e "${RED}Unit test 1 failed with code ${result} !${NC}"
+    echo -e "${RED}Unit test 1 failed with code ${http_code} !${NC}"
 fi
+
+# ----------------------------------------
+# Unit tests 2: get error 400 invalid mac
+# ----------------------------------------
+
+echo -e "${YELLOW}Running unit test 2: get error 400 invalid mac${NC}"
+
+echo -e "curl on http://api_hermes/v1/config/ac2350/invalid_mac..."
+
+
+http_code=$(curl -s -o /dev/null -w "%{http_code}" http://api_hermes:8000/v1/config/ac2350/invalid_mac)
+message=$(curl -s http://api_hermes:8000/v1/config/ac2350/invalid_mac)
+
+if [ $http_code -eq 400 ]; then
+    echo -e "${GREEN}Unit test 2 passed with with code: ${http_code} and message: ${message} !${NC}"
+else
+    echo -e "${RED}Unit test 2 failed with code: ${http_code} and message: ${message} !${NC}"
+fi
+
+# ----------------------------------------
+# Unit tests 3
+# ----------------------------------------
+
