5b29c3085f84c959f55a271ee1c3ca8c5433e19f
Léo Lefebvre <leo.lefebvre@telecom-paris.fr>
2024-05-21 21:57:44 +0200

    adding some things

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#I don't want to have the output of this next command in my console 
+# (docker network ls -q | xargs docker network rm) > /dev/null 2>&1
+
 #run commande to up docker compose
-docker-compose up -d --build
+docker-compose up -d --force-recreate --build
 
 #wait the end of the container unit_tests
 test=$(docker ps | grep unit_tests)
@@ -13,3 +16,4 @@ done
 #show logs of the container unit_tests
 docker logs unit_tests
 
+# (docker network ls -q | xargs docker network rm) > /dev/null 2>&1
