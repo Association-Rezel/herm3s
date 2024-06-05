e9aa5e91a43bf61dd0e404289295338aba7177b1
Antoine Quint <antoine.quint@telecom-paris.fr>
2024-06-05 15:01:17 +0200

    Enhancing and correcting code

--- hermes/api/MacAddress.py
+++ hermes/api/MacAddress.py
@@ -1,15 +1,15 @@
 from netaddr import EUI
 
-
-
 class MacAddress:
-    """Class to check if the mac address is valid and store it"""
+    """
+    Class to check if the mac address is valid and store it
+    """
 
     mac: str = None
 
     def __init__(self, mac: str):
         try:
-            self.___mac = str(EUI(mac)).replace("-", ":")
+            self.___mac = str(EUI(mac)).replace("-", ":") # Convert the mac address to the correct format
         except Exception as e:
             print(e)
             self.___mac = None
