27b0c231d565638accabfbf7f2f18e41be9f75f9
Nathan Roos <nathan.roos@telecom-paris.fr>
2024-06-24 11:40:31 +0000

    Added field "vlan" in unets/network/lan_ipv4 to store the index of the vlan used in the parameters of the box to emulate different lans

--- hermes/communication_db/env.py
+++ hermes/communication_db/env.py
@@ -13,14 +13,22 @@ if "CONFIG_PATH" in os.environ and os.path.exists(os.environ["CONFIG_PATH"]):
 
 if "DB_URI" in os.environ:
     DB_URI = os.environ["DB_URI"]
-elif CONFIG_FILE_EXISTS and "DB_URI" in config["communication_db"]:
+elif (
+    CONFIG_FILE_EXISTS
+    and config["communication_db"] is not None
+    and "DB_URI" in config["communication_db"]
+):
     DB_URI = config["communication_db"]["DB_URI"]
 else:
     DB_URI = "mongodb://admintest:Rwy4ygL5hqoNUr@137.194.13.162/?retryWrites=true&w=majority&authSource=test"
 
 if "DB_NAME" in os.environ:
     DB_NAME = os.environ["DB_NAME"]
-elif CONFIG_FILE_EXISTS and "DB_NAME" in config["communication_db"]:
+elif (
+    CONFIG_FILE_EXISTS
+    and config["communication_db"] is not None
+    and "DB_NAME" in config["communication_db"]
+):
     DB_NAME = config["communication_db"]["DB_NAME"]
 else:
     DB_NAME = "test"
